51c5fc26e5dc1d09882ba9a633688b79ae93a73f
Alexandru Tornea <tornea.alexandru@gmail.com>
2020-07-01 07:07:58 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,11 @@
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({battleId: '1699246'});
+  const armies = [
+      {name: "Nevskiy", battleId: 1708026},
+      {name: "Snow White", battleId: 1708025},
+      {name: "Agra", battleId: 1708023},
+      {name: "Snow White 5", battleId: 1708024}];
+  
+  chrome.storage.sync.set({armies});
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
